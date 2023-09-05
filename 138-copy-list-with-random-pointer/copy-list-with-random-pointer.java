22e3d8763cf3a79c1cdb09b042ed4c31504d9c98
AnuragK2 <110308448+AnuragK2@users.noreply.github.com>
2023-09-05 23:15:21 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (50.49%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
@@ -15,21 +15,48 @@ class Node {
 
 class Solution {
     public Node copyRandomList(Node head) {
-        if(head==null){
-            return null;
+        // if(head==null){
+        //     return null;
+        // }
+        // HashMap<Node,Node> mp=new HashMap<>();
+        // Node curr=head;
+        // while(curr!=null){
+        //     mp.put(curr,new Node(curr.val));
+        //     curr=curr.next;
+        // }
+        // curr=head;
+        // while(curr!=null){
+        //     mp.get(curr).next=mp.get(curr.next);
+        //     mp.get(curr).random=mp.get(curr.random);
+        //     curr=curr.next;
+        // }
+        // return mp.get(head);
+        Node itr=head;
+        Node front=head;
+        while(itr!=null){
+            front=itr.next;
+            Node copy=new Node(itr.val);
+            itr.next=copy;
+            copy.next=front;
+            itr=front;
         }
-        HashMap<Node,Node> mp=new HashMap<>();
-        Node curr=head;
-        while(curr!=null){
-            mp.put(curr,new Node(curr.val));
-            curr=curr.next;
+        itr=head;
+        while(itr!=null){
+            if(itr.random!=null){
+                itr.next.random=itr.random.next;
+            }
+            itr=itr.next.next;
         }
-        curr=head;
-        while(curr!=null){
-            mp.get(curr).next=mp.get(curr.next);
-            mp.get(curr).random=mp.get(curr.random);
-            curr=curr.next;
+        Node dummy=new Node(0);
+        Node cp=dummy;
+        itr=head;
+        while(itr!=null){
+            front=itr.next.next;
+            cp.next=itr.next;
+            itr.next=front;
+            cp=cp.next;
+            itr=itr.next;
         }
-        return mp.get(head);
+        return dummy.next;
     }
 }
