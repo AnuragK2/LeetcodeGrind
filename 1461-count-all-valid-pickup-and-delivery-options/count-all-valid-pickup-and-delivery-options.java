ee84f2b5de53d72eb04e6eb1920407f59e90070d
AnuragK2 <110308448+AnuragK2@users.noreply.github.com>
2023-09-11 03:29:41 +0530

    Time: 0 ms (100.00%) | Memory: 39.7 MB (45.51%) - LeetSync

--- 1461-count-all-valid-pickup-and-delivery-options/count-all-valid-pickup-and-delivery-options.java
+++ 1461-count-all-valid-pickup-and-delivery-options/count-all-valid-pickup-and-delivery-options.java
@@ -1,5 +1,5 @@
 class Solution {
-    public static int solve(int n){
+    public static int solve(int n,int[] dp){
         int mod=(int)Math.pow(10,9)+7;
         if(n==1){
             return 1;
@@ -7,12 +7,17 @@ class Solution {
         if(n==2){
             return 6;
         }
-        long ans=solve(n-1);
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        long ans=solve(n-1,dp);
         int oddno=2*(n-1)+1;
         long totalperm=(long)(((long)oddno*(oddno+1))/2)%mod;
-        return (int)((ans*totalperm)%mod);
+        return dp[n]=(int)((ans*totalperm)%mod);
     }
     public int countOrders(int n) {
-        return solve(n);
+        int[] dp=new int[n+1];
+        Arrays.fill(dp,-1);
+        return solve(n,dp);
     }
 }
